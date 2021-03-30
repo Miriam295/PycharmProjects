53bc585f2584541bd4d811a179bbc0ff376eb6b7
Miriam295 <66078074+Miriam295@users.noreply.github.com>
2021-03-30 17:02:50 +0200

    prime numbers first commit

--- tasks/prime_numbers.py
+++ tasks/prime_numbers.py
@@ -1,4 +1,16 @@
-n = 10
-print(n)
+#How many prime numbers are wanted?
+n = 10 #specify n
 
 #task: write a program that prints the first n prime numbers
+prime = []
+for i in range(1, 10000000000000000000000000):
+    for j in range (2, i):
+        if i % j == 0:
+            break
+        else:
+            prime.append(i)
+            break
+    if(len(prime) == n):
+        break
+
+print("The first", n, "prime numbers are:", prime)
