05921bd28eb0e7ac2ff29f28c9315064b67c032d
Miriam295 <66078074+Miriam295@users.noreply.github.com>
2021-04-07 13:09:11 +0200

    prime numbers third commit

--- tasks/prime_numbers.py
+++ tasks/prime_numbers.py
@@ -1,9 +1,9 @@
 #How many prime numbers are wanted?
-n = 10 #specify n
+n = 14 #specify n
 
 #task: write a program that prints the first n prime numbers
 prime = []
-for i in range(1, 1000000000000000000): #comment: I could not find a solution to run from 1 to infinity
+for i in range(1, 10000000000000000000000): #comment: I could not find a solution to run from 1 to infinity
     for j in range (2, i):
         if i % j == 0:
             break
