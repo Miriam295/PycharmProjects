ba5eb026c2af1eb642e32c3df2fefff4867ab20c
Miriam295 <66078074+Miriam295@users.noreply.github.com>
2021-05-06 02:30:40 +0200

    stock data test

--- tasks/stock_data_new.py
+++ tasks/stock_data_new.py
@@ -1,20 +1,25 @@
-#Program that searches for Stock data files
+import os
 import csv
-
-#for ... if file is csv and if row 1 line 1 = "date" --> program; else pass
-
-#Calculate percentage change (for each of the found files)
-    #change = (close-open) / open
-import os.path
-print(os.path.exists('IBM.csv'))
-
-with open('TSLA.csv', 'r') as stock:
-    read = csv.reader(stock)
-    lines = stock.readlines()
-    for line in lines:
-        i = i + 1
-        print("%i: %s" % (i, line))
-
-#filename.append for changing?
-
-#Save output files to another folder
+from csv import writer
+from csv import reader
+#define function to add columns
+def add_column_in_csv(input_file, output_file, transform_row):
+    with open(input_file, 'r') as read_obj, \
+            open(output_file, 'w', newline='') as write_obj:
+        csv_reader = reader(read_obj)
+        csv_writer = writer(write_obj)
+        for row in csv_reader:
+            transform_row(row, csv_reader.line_num)
+            csv_writer.writerow(row)
+#search for stock data files
+stock_head = ('Date,Open,High,Low,Close,Adj Close,Volume')
+directory = os.path.join("c:\\", input("Enter the folder to search in: "))
+for root,dirs,files in os.walk(directory):
+    for file in files:
+       if file.endswith(".csv"):
+           with open(file, 'r') as stock:
+               reader1 = csv.reader(stock)
+               if next(reader1) != stock_head:
+                   continue
+               else:
+                   #start calculation
