daa4366f5b3ad355d8a420be623c2a20e4418b1a
Miriam295 <66078074+Miriam295@users.noreply.github.com>
2021-05-07 10:21:22 +0200

    stock data Stand

--- tasks/stock_data_new.py
+++ tasks/stock_data_new.py
@@ -1,18 +1,50 @@
-import os
-import csv
+#I sadly couldnt finalize this
+#What's still missing:
+#(I basically only finished assignment 2 and started the rest with flaws):
+    #Calculation is only saved for last file in loop (as it currently overwrites the old output)
+    #and I tried adding the old data + new column together with next() and .append, but:
+        #the first two rows somehow disappeared;
+        #the calculated change does not change with the rows
 
-#search for stock data files
-stock_head = ('Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume')
-directory = os.path.join("c:\\", input("Enter the folder to search in: "))
-for files in os.walk(directory):
-    for file in files:
-        if file.endswith(".csv"):
-            with open(file, 'r') as stock:
-                reader1 = csv.reader(stock)
-                if next(reader1) != stock_head:
-                    continue
-                else:
-                    with open (file, 'a'):
-                        reader2 = csv.reader(stock)
-                        for line in reader1: ###need to somehow skip row 1
-                            line.append(((line[4] - line[1]) / line[1]))
+#search for stock data files and apply function (completed and works)
+import csv
+from pathlib import Path
+
+csv_data = Path(input("Enter the full path to search in: ")).rglob('*.csv')
+files = [x for x in csv_data]
+stock_files = []
+for file in files:
+    file_str = str(file)
+    if file_str.endswith(".csv"):
+        with open(file_str, 'r') as stock:
+            reader1 = csv.reader(stock)
+            stock_head = str(['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
+            test = str(next(reader1))
+            if test == stock_head:
+                stock_files.append(file)
+
+#calculating change rate and adding it to new column (presents a result but is wrong)
+for el in stock_files:
+    print(el)
+    with open(el, 'r') as input:
+        with open('stock_with_change.csv', 'w', newline = '') as output:
+            reader = csv.reader(input)
+            writer = csv.writer(output)
+            i = 0
+            change_collect = []
+            for line in reader:
+                i += 1
+                if i > 1:
+                    for k in range(1, 7):
+                        line[k] = float(line[k])
+                    change = ((line[4] - line[1]) / line[1])
+                    change_collect += [change]
+                    all = []
+                    row = next(reader)
+                    row.append('Change')
+                    all.append(row)
+                    for row in reader:
+                        for row2 in change_collect:
+                            row.append(row2)
+                            all.append(row)
+            writer.writerows(all)
